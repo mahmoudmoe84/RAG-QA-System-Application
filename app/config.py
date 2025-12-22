134c0166dd68e391ceac85e5f95a3ca1083b238d
mahmoudmoe84 <mahmoud.moawed@gmail.com>
2025-12-22 15:48:49 +0400

    activating langsmith

--- app/config.py
+++ app/config.py
@@ -7,7 +7,9 @@ from pydantic_settings import BaseSettings , SettingsConfigDict
 class Settings(BaseSettings):
     """Application config setting loaded from environment variables."""
     
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8",)
+    model_config = SettingsConfigDict(env_file=".env", 
+                                      env_file_encoding="utf-8",
+                                      extra="ignore")
     
     #Open AI Config 
     OPENAI_API_KEY: str
@@ -38,6 +40,13 @@ class Settings(BaseSettings):
     ragas_llm_model: str | None = None  # Defaults to llm_model if not set
     ragas_llm_temperature: float | None = None  # Defaults to llm_temperature if not set
     ragas_embedding_model: str | None = None  # Defaults to embedding_model if not set
+    
+    # #adding langsmith config
+    # LANGSMITH_API_KEY: str
+    # LANGSMITH_PROJECT: str 
+    # LANGSMITH_TRACING: str 
+    # LANGSMITH_ENDPOINT: str
+    
     
     # API settings
     API_HOST: str = "0.0.0.0"
